036a6c4c93fd380b30e759d897c128bf50536bb2
alan_omam <omamalan@hotmail.it>
2024-02-12 17:44:16 +0100

    impostazione snack 3

--- SNACK2/index.js
+++ SNACK2/index.js
@@ -5,3 +5,31 @@ Scrivi una funzione che fonda due array presi come parametri (date per scontato 
 es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
  */
+
+let array1 = ["a","b","c"]
+let array2 = [1,2,3]
+let concatArray = array1.concat(array2)
+let mixArray = []
+
+
+/*for (let i =0; i<(array1.length+array2.length); i++) {
+
+    
+    mixArray.push(array1[i][i])
+}*/
+
+
+for (let i =0; i<array1.length ; i++) {
+    mixArray.push(concatArray[i])
+    
+    for (let j =0; j<array2.length; j++) {
+
+        mixArray[i].push(concatArray[i])
+     } 
+   
+ } 
+
+ 
+
+
+console.log(mixArray)
